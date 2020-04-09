cb6f1a194ec38da3dc0b2a80e6d68bf560825c06
ansarirayyan <29453512+ansarirayyan@users.noreply.github.com>
2020-04-09 00:51:32 -0700

    fixed a bug that occured when first file in dir was vid rather than static img

---
+++ README.md
@@ -0,0 +1,2 @@
+# Overview
+[InstaBot](https://github.com/ansarirayyan/InstaBot) is a Discord bot made using [discord.py](https://pypi.org/project/discord.py/) that displays images from Instagram via [`instagram-scraper`](https://github.com/rarcega/instagram-scraper).
--- main.py
+++ main.py
@@ -2,31 +2,7 @@ import os
 import discord
 from getpass import getpass
 from discord.ext import commands
-
-#usrFile = open("usr", "r+")
-#passwdFile = open("passwd", "r+")
-
-#if '' == usrFile.read:
-#    print ("What is your Instagram username?")
-#    input(usr)
-
-#class MyClient(discord.Client):
-
-    # set-up the program
-#    usr = input("What is your Instagram username?\n")
-#    passwd = getpass("What is the password of your Instagram account?\n")
-
-#    async def on_ready(self):
-#        print('Logged on as', self.user)
-
-#    async def on_message(self, message):
-        # don't respond to ourselves
-#        if message.author == self.user:
-#            return
-
-    #    if message.content == 'insta':
-    #        os.system("instagram-scraper omarmwaseem -u " + self.usr + " -p " + self.passwd)
-    #        await client.send_file(message.channel, "./omarmwaseem/82748276_855403461561603_6777149114328894202_n.jpg")
+#from filetype import filetype
 
 bot = commands.Bot(command_prefix='insta ')
 
@@ -39,21 +15,61 @@ async def on_ready():
 
     print(f'Logged in as {bot.user.name} on {len(bot.guilds)} server(s).')
 
-# THIS WORKS
-#@bot.command()
-#async def test(ctx, arg):
-#    await ctx.send("Processing your request... ")
-#    await ctx.send(arg)
-#    await ctx.send(file=discord.File('./omarmwaseem/82748276_855403461561603_6777149114328894202_n.jpg'))
-
 @bot.command()
 async def usr(ctx, arg):
     await ctx.send("Processing your request... ")
     print("Starting to fetch data... ")
     os.system("instagram-scraper " + arg + " -u " + usr_ + " -p " + passwd)
     print("Done.")
+    i = 0
+    repeat = True
+    while(repeat == True):
+        try:
+            await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+            repeat = False
+        except:
+            i = i + 1
+            try:
+                await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+                repeat = False
+            except:
+                i = i + 1
+                await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+                repeat = True
 
-    await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[0]))
+@bot.command()
+async def N(ctx, arg):
+    i = i + 1
+    repeat = True
+    while(repeat == True):
+        try:
+            await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+            repeat = False
+        except:
+            i = i + 1
+            try:
+                await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+                repeat = False
+            except:
+                i = i + 1
+                await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[i]))
+                repeat = True
+
+#@bot.command()
+#async def usr(ctx, arg):
+#    await ctx.send("Processing your request... ")
+#    print("Starting to fetch data... ")
+#    #os.system("instagram-scraper " + arg + " -u " + usr_ + " -p " + passwd)
+#    print("Done.")
+#    print(os.listdir("./" + arg)[0])
+#    kind = filetype.guess('./' + arg + '/' + os.listdir("./" + arg)[0])
+#    #print(kind.extension)
+#    print('File extension: %s' % kind.extension)
+#    if kind is not image/jpeg:
+#        pass;
+#    await ctx.send(file=discord.File('./' + arg + '/' + os.listdir("./" + arg)[0]))
+
+
 
 #client = MyClient()
-bot.run('')
+bot.run('Njk3NjUxMzc3NjY0NTU3MTA4.Xo6YhA.hUmgnBJggnhWszkl5zXEgf5Irpw')
