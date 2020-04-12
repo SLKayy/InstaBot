c5240830b29d0b84d4e7f2c1fef5dd2b38c62714
ansarirayyan <29453512+ansarirayyan@users.noreply.github.com>
2020-04-12 10:47:53 -0700

    ok stp

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 [InstaBot](https://github.com/ansarirayyan/InstaBot) is a [Discord](https://discordapp.com/) bot made using [discord.py](https://pypi.org/project/discord.py/) that displays images from Instagram via [`instagram-scraper`](https://github.com/rarcega/instagram-scraper).
 
 # Usage
-In a Discord chat, post `insta usr <Instagram username>` to get the first or so image that the specified user has posted into the server. To go to the next image, simply run `insta N`.
+Here lies a list of commands that can be used to operate the bot:
+
 
 # Installation
-So that this script can download the appropriate files, install [`instagram-scraper`]. Please refer to their simple installation guide [here](https://github.com/rarcega/instagram-scraper/blob/master/README.md#install). Then install the discord.py libraries. More information can be found [here](https://pypi.org/project/discord.py/). All the other modules used should already be installed, but if not, then simply install them `pip` or something when errored out.
+So that this script can download the appropriate files, install [`instagram-scraper`]. Please refer to their simple installation guide [here](https://github.com/rarcega/instagram-scraper/blob/master/README.md#install). Then install the discord.py libraries. More information can be found [here](https://pypi.org/project/discord.py/). All the other modules used should already be installed, but if not, then simply install them via `pip` (or `git`, or another alternative method of installation) when errored out. This program is written for Python 3.
+
+# Running
+UNIX or UNIX-liked:
+```
+python3 main.py
+```
+
+On Windows:
+```
+py main.py
+```
 
 # Behavior
-As Discord doesn't allow to upload large videos, only static images are displayed. Something peculiar I've noticed during testing is that the software sometimes even skips the first few images, as is the case when pulling from Instagram user [omarmwaseem](https://www.instagram.com/omarmwaseem/).
+As Discord doesn't allow to upload large videos, only static images are displayed. Something peculiar I've noticed during testing is that the software sometimes even skips the first few images, as is the case when pulling from Instagram user [omarmwaseem](https://www.instagram.com/omarmwaseem/). Also, as of the time of the writing of this document, `instagram-scraper` does not allow the use of non-UTF-8 in searches, but there is an [open issue](https://github.com/rarcega/instagram-scraper/issues/83) (though outstanding for a couple of years) on its GitHub repository.
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def usr(ctx, arg):
             i = i + 1
             repeat = True
         else:
-            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ")
+            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ansarirayyan's InstaBot GitHub repo")
             i = i + 1
             repeat = True
 
@@ -58,27 +58,54 @@ async def N(ctx, *arg):
             i = i + 1
             repeat = True
         else:
-            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ")
+            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ansarirayyan's InstaBot GitHub repo")
             i = i + 1
             repeat = True
 
-#@bot.command()
-#async def B(ctx, *arg):
-#    global i
-#    global usrCmdArg
-#    i = i + 1
-#    repeat = True
-#    while(repeat == True):
-#        if str(os.listdir("./" + usrCmdArg)[i]).endswith('.jpg'):
-#            await ctx.send(file=discord.File('./' + usrCmdArg + '/' + os.listdir("./" + usrCmdArg)[i]))
-#            repeat = False
-#        elif str(os.listdir("./" + usrCmdArg)[i]).endswith('.mp4'):
-#            print("File is not of JPEG type, skipping... ")
-#            i = i - 1
-#            repeat = True
-#        else:
-#            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ")
-#            i = i - 1
-#            repeat = True
+@bot.command()
+async def tag(ctx, arg, maxNumb='10'):
+    global i
+    global usrCmdArg
+    usrCmdArg = arg + "_tag"
+    await ctx.send("Processing your request... ")
+    print("Starting to fetch data... ")
+    if (os.path.isdir("./" + arg + "_tag")):
+        pass
+    else:
+        os.system("instagram-scraper " + arg + " --tag -m " + maxNumb)
+        os.rename(arg, arg + "_tag") # just in case if there's a username with the same tag name
+    print("Done.")
+    repeat = True
+    while(repeat == True):
+        if str(os.listdir("./" + arg + "_tag")[i]).endswith('.jpg'):
+            await ctx.send(file=discord.File('./' + arg + '_tag' + '/' + os.listdir("./" + arg + "_tag")[i]))
+            repeat = False
+        elif str(os.listdir("./" + arg + "_tag")[i]).endswith('.mp4'): # can videos be tagged?
+            print("File is not of JPEG type, skipping... ")
+            i = i + 1
+            repeat = True
+        else:
+            print("Some other file type than *.mp4 and *.jpg is in this directory. Please post this in the \"Issues\" tab of ansarirayyan's InstaBot GitHub repo")
+            i = i + 1
+            repeat = True
 
-bot.run('')
+@bot.command()
+async def reload_usr(ctx, arg): # to get the latest posts
+    os.system("instagram-scraper " + arg + " -u " + usr_ + " -p " + passwd)
+
+@bot.command()
+async def reload_tag(ctx, arg, maxNumb): # will this fetch the latest or the oldest? only time will tell
+    os.system("instagram-scraper " + arg + " --tag -m " + maxNumb)
+    os.rename(arg, arg + "_tag")
+
+@bot.command()
+async def ls(ctx, *arg):
+    p = os.listdir("./")
+    for i1 in p:
+        if os.path.isdir(i1):
+            if (i1 == ".git"):
+                pass
+            else:
+                await ctx.send(i1)
+
+bot.run('Njk3NjUxMzc3NjY0NTU3MTA4.Xo7USA.vr_icsKwonChN8M3xMFVdoiae_I')
